365a54e63b952064dad9c7320eeeb5cdaad6ccc0
Hiroki321996 <hiroki.tani@rakus-partners.co.jp>
2020-02-05 12:00:28 +0900

    insertメソッドを追加しました

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 /**
@@ -19,5 +20,7 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository repository;
 	
-	
+	public void insert(Administrator administrator) {
+		repository.insert(administrator);
+	}
 }
