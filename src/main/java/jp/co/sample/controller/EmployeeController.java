2121558083b155488db03d1396d9d861fc6ee4d2
Hiroki321996 <hiroki.tani@rakus-partners.co.jp>
2020-02-05 16:33:41 +0900

    BeanUtilsを使おうと思ってインポートしていましたが、使わなかったので消しました

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -2,7 +2,6 @@ package jp.co.sample.controller;
 
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
